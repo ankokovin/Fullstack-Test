210ecafaa397d9fbd9ba9349a778275c129056ed
Коковин Алексей Николаевич <ankokovin@edu.hse.ru>
2020-06-25 19:16:56 +0500

    Add TODO(#8)

--- WebServer/src/main/java/ankokovin/fullstacktest/WebServer/WebServerApplication.java
+++ WebServer/src/main/java/ankokovin/fullstacktest/WebServer/WebServerApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WebServerApplication {
 	// TODO(#4): настройка портов и https
-	// TODO: модель организации
+	// TODO(#8): модель организации
 	// TODO: модель работников
 	// TODO: генерация кода с jOOQ 
 	// TODO: контроллер организаций
