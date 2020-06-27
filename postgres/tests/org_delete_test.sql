e7c28570b0ba8282488f5e5bbeb8767f6885b2c2
Kokovin Aleksei <rycarok@gmail.com>
2020-06-27 18:12:36 +0500

    Добавил проверок и фикс rollback

--- postgres/tests/org_delete_test.sql
+++ postgres/tests/org_delete_test.sql
@@ -2,7 +2,7 @@
 \set ON_ERROR_STOP true
 BEGIN;
 
-SELECT plan(4);
+SELECT plan(7);
 
 PREPARE org_insert AS INSERT INTO Organization VALUES
     (1, 'headOrg1', NULL),
@@ -25,4 +25,17 @@ PREPARE del_head_org AS DELETE FROM Organization WHERE id = 1;
 SELECT throws_ok (
     'del_head_org'
 );
-END;
+SELECT results_eq(
+    'SELECT * FROM Organization',
+     $$VALUES ( 1, 'headOrg1', NULL), (2, 'headOrg2', NULL), (4, 'Org_1_2', 1)$$
+);
+INSERT INTO Worker VALUES (1, 'worker1', 4, NULL);
+PREPARE del_org_with_worker AS DELETE FROM Organization WHERE id = 1;
+SELECT throws_ok (
+    'del_org_with_worker'
+);
+SELECT results_eq(
+    'SELECT * FROM Organization',
+     $$VALUES ( 1, 'headOrg1', NULL), (2, 'headOrg2', NULL), (4, 'Org_1_2', 1)$$
+);
+ROLLBACK;
