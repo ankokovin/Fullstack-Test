fcbceedbce4f84738ea3fd9efa31979d77e55429
Kokovin Aleksei <rycarok@gmail.com>
2020-06-28 02:59:44 +0500

    implement #17

--- postgres/tests/worker_test.sql
+++ postgres/tests/worker_test.sql
@@ -1,11 +1,8 @@
 \set ON_ERROR_ROLLBACK 1
 \set ON_ERROR_STOP true
 BEGIN;
-SELECT plan(18);
-SELECT rules_are(
-    'worker',
-    ARRAY[ 'on_insert', 'on_update']
-);
+SELECT plan(17);
+
 INSERT INTO Organization VALUES
     (1, 'headOrg1', NULL),
     (2, 'headOrg2', NULL),
@@ -20,15 +17,18 @@ PREPARE insert_workers AS INSERT INTO Worker VALUES
     (5, 'Worker5', 2, 4),
     (6, 'Worker6', 3, NULL),
     (7, 'Worker7', 4, NULL);
-
+-- 1
 SELECT lives_ok(
     'insert_workers'
 );
 
 PREPARE delete_worker AS DELETE FROM Worker WHERE id = 6;
+
+-- 2
 SELECT lives_ok(
     'delete_worker'
 );
+-- 3
 SELECT results_eq(
     'SELECT * FROM Worker',
      $$VALUES  (1, 'Worker1', 1, NULL),
@@ -40,9 +40,11 @@ SELECT results_eq(
 );
 
 PREPARE delete_head_worker AS DELETE FROM Worker WHERE id = 4;
+-- 4
 SELECT throws_ok(
     'delete_head_worker'
 );
+-- 5
 SELECT results_eq(
     'SELECT * FROM Worker',
      $$VALUES  (1, 'Worker1', 1, NULL),
@@ -54,9 +56,11 @@ SELECT results_eq(
 );
 
 PREPARE insert_worker AS INSERT INTO Worker VALUES (8, 'Worker8', 1, 1);
+-- 6
 SELECT lives_ok(
     'insert_worker'
 );
+-- 7
 SELECT results_eq(
     'SELECT * FROM Worker',
      $$VALUES  (1, 'Worker1', 1, NULL),
@@ -69,7 +73,9 @@ SELECT results_eq(
 );
 
 PREPARE insert_worker_wrong_org AS INSERT INTO Worker VALUES (9, 'Worker9', 1, 3);
+-- 8
 SELECT throws_ok('insert_worker_wrong_org');
+-- 9
 SELECT results_eq(
     'SELECT * FROM Worker',
      $$VALUES  (1, 'Worker1', 1, NULL),
@@ -82,7 +88,9 @@ SELECT results_eq(
 );
 
 PREPARE insert_worker_selfref AS INSERT INTO Worker VALUES (10, 'Worker10', 1, 9);
+-- 10
 SELECT throws_ok('insert_worker_selfref');
+-- 11
 SELECT results_eq(
     'SELECT * FROM Worker',
      $$VALUES  (1, 'Worker1', 1, NULL),
@@ -98,9 +106,11 @@ PREPARE update_worker AS
     UPDATE Worker
     SET (id, worker_name, org_id, head_id) = (8, 'Worker_new', 2, 3)
     WHERE id = 8;
+-- 12
 SELECT lives_ok(
     'update_worker'
 );
+-- 13
 SELECT results_eq(
     'SELECT * FROM Worker',
      $$VALUES  (1, 'Worker1', 1, NULL),
@@ -109,14 +119,16 @@ SELECT results_eq(
     (4, 'Worker4', 2, 3),
     (5, 'Worker5', 2, 4),
     (7, 'Worker7', 4, NULL),
-    (8, 'Worker8', 2, 3)$$
+    (8, 'Worker_new', 2, 3)$$
 );
 
 PREPARE update_worker_wrong_org AS
     UPDATE Worker
     SET (id, worker_name, org_id, head_id) =  (8, 'Worker9', 1, 3)
     WHERE id = 8;
+-- 14
 SELECT throws_ok('update_worker_wrong_org');
+-- 15
 SELECT results_eq(
     'SELECT * FROM Worker',
      $$VALUES  (1, 'Worker1', 1, NULL),
@@ -125,14 +137,16 @@ SELECT results_eq(
     (4, 'Worker4', 2, 3),
     (5, 'Worker5', 2, 4),
     (7, 'Worker7', 4, NULL),
-    (8, 'Worker8', 2, 3)$$
+    (8, 'Worker_new', 2, 3)$$
 );
 
 PREPARE update_worker_selfref AS
     UPDATE Worker
     SET (id, worker_name, org_id, head_id) = (8, 'Worker10', 1, 8)
     WHERE id = 8;
+-- 16
 SELECT throws_ok('update_worker_selfref');
+-- 17
 SELECT results_eq(
     'SELECT * FROM Worker',
      $$VALUES  (1, 'Worker1', 1, NULL),
@@ -141,7 +155,7 @@ SELECT results_eq(
     (4, 'Worker4', 2, 3),
     (5, 'Worker5', 2, 4),
     (7, 'Worker7', 4, NULL),
-    (8, 'Worker8', 2, 3)$$
+    (8, 'Worker_new', 2, 3)$$
 );
 
 ROLLBACK;
