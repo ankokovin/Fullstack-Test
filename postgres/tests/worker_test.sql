1530de7d0caa79b308cb8569e1764dafbd2f55b9
Kokovin Aleksei <rycarok@gmail.com>
2020-06-27 20:25:28 +0500

    add 'update' tests

--- postgres/tests/worker_test.sql
+++ postgres/tests/worker_test.sql
@@ -1,7 +1,7 @@
 \set ON_ERROR_ROLLBACK 1
 \set ON_ERROR_STOP true
 BEGIN;
-SELECT plan(12);
+SELECT plan(18);
 SELECT rules_are(
     'worker',
     ARRAY[ 'on_insert', 'on_update']
@@ -92,6 +92,57 @@ SELECT results_eq(
     (5, 'Worker5', 2, 4),
     (7, 'Worker7', 4, NULL),
     (8, 'Worker8', 1, 1)$$
+);
+
+-- TODO: такие же случаи при Update
+PREPARE update_worker AS
+    UPDATE Worker
+    SET (id, worker_name, org_id, head_id) = (8, 'Worker_new', 2, 3)
+    WHERE id = 8;
+SELECT lives_ok(
+    'update_worker'
+);
+SELECT results_eq(
+    'SELECT * FROM Worker',
+     $$VALUES  (1, 'Worker1', 1, NULL),
+    (2, 'Worker2', 1, 1),
+    (3, 'Worker3', 2, NULL),
+    (4, 'Worker4', 2, 3),
+    (5, 'Worker5', 2, 4),
+    (7, 'Worker7', 4, NULL),
+    (8, 'Worker8', 2, 3)$$
+);
+
+PREPARE update_worker_wrong_org AS
+    UPDATE Worker
+    SET (id, worker_name, org_id, head_id) =  (8, 'Worker9', 1, 3)
+    WHERE id = 8;
+SELECT throws_ok('update_worker_wrong_org');
+SELECT results_eq(
+    'SELECT * FROM Worker',
+     $$VALUES  (1, 'Worker1', 1, NULL),
+    (2, 'Worker2', 1, 1),
+    (3, 'Worker3', 2, NULL),
+    (4, 'Worker4', 2, 3),
+    (5, 'Worker5', 2, 4),
+    (7, 'Worker7', 4, NULL),
+    (8, 'Worker8', 2, 3)$$
+);
+
+PREPARE update_worker_selfref AS
+    UPDATE Worker
+    SET (id, worker_name, org_id, head_id) = (8, 'Worker10', 1, 8)
+    WHERE id = 8;
+SELECT throws_ok('update_worker_selfref');
+SELECT results_eq(
+    'SELECT * FROM Worker',
+     $$VALUES  (1, 'Worker1', 1, NULL),
+    (2, 'Worker2', 1, 1),
+    (3, 'Worker3', 2, NULL),
+    (4, 'Worker4', 2, 3),
+    (5, 'Worker5', 2, 4),
+    (7, 'Worker7', 4, NULL),
+    (8, 'Worker8', 2, 3)$$
 );
 
 ROLLBACK;
