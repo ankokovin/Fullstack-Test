84e60950165a2d8dea745c30b01128420731ac30
Kokovin Aleksei <rycarok@gmail.com>
2020-06-29 01:35:58 +0500

    добавил тест на добавление и изменение организации на null

--- postgres/tests/worker_test.sql
+++ postgres/tests/worker_test.sql
@@ -2,7 +2,7 @@ CREATE EXTENSION IF NOT EXISTS pgtap;
 \set ON_ERROR_ROLLBACK 1
 \set ON_ERROR_STOP true
 BEGIN;
-SELECT plan(17);
+SELECT plan(21);
 
 INSERT INTO Organization VALUES
     (1, 'headOrg1', NULL),
@@ -103,15 +103,30 @@ SELECT results_eq(
     (8, 'Worker8', 1, 1)$$
 );
 
+PREPARE insert_worker_org_null AS INSERT INTO Worker VALUES (10, 'Worker10', NULL, NULL);
+-- 12
+SELECT throws_ok('insert_worker_org_null');
+-- 13
+SELECT results_eq(
+    'SELECT * FROM Worker',
+     $$VALUES  (1, 'Worker1', 1, NULL),
+    (2, 'Worker2', 1, 1),
+    (3, 'Worker3', 2, NULL),
+    (4, 'Worker4', 2, 3),
+    (5, 'Worker5', 2, 4),
+    (7, 'Worker7', 4, NULL),
+    (8, 'Worker8', 1, 1)$$
+);
+
 PREPARE update_worker AS
     UPDATE Worker
     SET (id, worker_name, org_id, head_id) = (8, 'Worker_new', 2, 3)
     WHERE id = 8;
--- 12
+-- 14
 SELECT lives_ok(
     'update_worker'
 );
--- 13
+-- 15
 SELECT results_eq(
     'SELECT * FROM Worker',
      $$VALUES  (1, 'Worker1', 1, NULL),
@@ -127,9 +142,9 @@ PREPARE update_worker_wrong_org AS
     UPDATE Worker
     SET (id, worker_name, org_id, head_id) =  (8, 'Worker9', 1, 3)
     WHERE id = 8;
--- 14
+-- 16
 SELECT throws_ok('update_worker_wrong_org');
--- 15
+-- 17
 SELECT results_eq(
     'SELECT * FROM Worker',
      $$VALUES  (1, 'Worker1', 1, NULL),
@@ -145,9 +160,27 @@ PREPARE update_worker_selfref AS
     UPDATE Worker
     SET (id, worker_name, org_id, head_id) = (8, 'Worker10', 1, 8)
     WHERE id = 8;
--- 16
+-- 18
 SELECT throws_ok('update_worker_selfref');
--- 17
+-- 19
+SELECT results_eq(
+    'SELECT * FROM Worker',
+     $$VALUES  (1, 'Worker1', 1, NULL),
+    (2, 'Worker2', 1, 1),
+    (3, 'Worker3', 2, NULL),
+    (4, 'Worker4', 2, 3),
+    (5, 'Worker5', 2, 4),
+    (7, 'Worker7', 4, NULL),
+    (8, 'Worker_new', 2, 3)$$
+);
+
+PREPARE update_worker_org_null AS
+    UPDATE Worker
+    SET (id, worker_name, org_id, head_id) = (8, 'Worker10',NULL,NULL)
+    WHERE id = 8;
+-- 20
+SELECT throws_ok('update_worker_org_null');
+-- 21
 SELECT results_eq(
     'SELECT * FROM Worker',
      $$VALUES  (1, 'Worker1', 1, NULL),
