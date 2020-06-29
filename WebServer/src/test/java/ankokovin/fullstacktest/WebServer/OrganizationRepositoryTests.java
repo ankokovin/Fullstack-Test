4fc658ad8f8f6081d03c9fdb36e73a6aa19ec34b
Kokovin Aleksei <rycarok@gmail.com>
2020-06-30 00:49:24 +0500

    create Update OrganizationRepository tests

--- WebServer/src/test/java/ankokovin/fullstacktest/WebServer/OrganizationRepositoryTests.java
+++ WebServer/src/test/java/ankokovin/fullstacktest/WebServer/OrganizationRepositoryTests.java
@@ -46,27 +46,38 @@ public class OrganizationRepositoryTests {
         dslContext.truncateTable(organization).restartIdentity().cascade().execute();
     }
 
+    public Organization[] create(Integer cnt) throws Exception {
+        String name = "ООО Тест%d";
+        Organization[] expected = new Organization[cnt];
+
+
+        for (int i=0; i<cnt; ++i) {
+            String fName = String.format(name,i);
+            int id = organizationRepository.insert(fName, null);
+            assertEquals(i+1, id);
+            expected[i] = new Organization(id, fName, null);
+        }
+        Object[] actual = dslContext
+                .selectFrom(organization)
+                .fetchInto(Organization.class).toArray();
+        assertArrayEquals(expected, actual);
+        return expected;
+    }
+
+    public Organization create() throws Exception {
+        return create(1)[0];
+    }
+
     @Nested
     class Insert {
         @Test
-        public void whenNoHead_thenOrganizationCreates() throws SameNameException {
-            String name = "ООО Тест";
-            Organization expected = new Organization(1, name, null);
-
-            int id = organizationRepository.insert(name, null);
-            assertEquals(1, id);
-
-
-            Object[] actual = dslContext
-                    .selectFrom(organization)
-                    .fetchInto(Organization.class).toArray();
-            assertArrayEquals(new Organization[]{expected}, actual);
+        public void whenNoHead_thenOrganizationCreates() throws Exception {
+            create();
         }
 
         @Test
-        public void whenHead_thenOrganizationCreates() throws SameNameException {
-            whenNoHead_thenOrganizationCreates();
-            Organization given = new Organization(1, "ООО Тест", null);
+        public void whenHead_thenOrganizationCreates() throws Exception {
+            Organization given = create();
             String name = "ООО Тест2";
             Organization expected = new Organization(2, name, 1);
             int id = organizationRepository.insert(name, 1);
@@ -86,9 +97,8 @@ public class OrganizationRepositoryTests {
         }
 
         @Test
-        public void whenWrongHead_thenThrows() throws SameNameException {
-            whenNoHead_thenOrganizationCreates();
-            Organization given = new Organization(1, "ООО Тест", null);
+        public void whenWrongHead_thenThrows() throws Exception {
+            Organization given = create();
             WrongHeadIdException e = assertThrows(WrongHeadIdException.class,
                     () -> organizationRepository.insert("OOO Тест2",3));
             assertEquals(2, e.id);
@@ -102,12 +112,69 @@ public class OrganizationRepositoryTests {
         }
 
         @Test
-        public void whenSameName_thenThrows() throws SameNameException {
-            whenNoHead_thenOrganizationCreates();
-            Organization given = new Organization(1, "ООО Тест", null);
+        public void whenSameName_thenThrows() throws Exception {
+            Organization given = create();
             SameNameException e = assertThrows(SameNameException.class,
                     () -> organizationRepository.insert(given.getOrgName(),null));
             assertEquals(given.getOrgName(), e.name);
+        }
+    }
+
+    @Nested
+    class Update {
+        @Test
+        public void whenChangeName_thenOrganizationUpdates() throws Exception {
+            Organization given = create();
+            String name = "ООО Тест Изменено";
+            Organization expected = new Organization(1, name, null);
+
+            int id = organizationRepository.update(given.getId(), name, null);
+            assertEquals(1, id);
+
+            Object[] actual = dslContext
+                    .selectFrom(organization)
+                    .fetchInto(Organization.class).toArray();
+            assertArrayEquals(new Organization[]{expected}, actual);
+        }
+
+        @Test
+        public void whenChangeHead_thenOrganizationUpdates() throws Exception {
+            Organization[] given = create(2);
+            Organization expected = new Organization(given[1].getId(), given[1].getOrgName(), given[0].getId());
+            int id = organizationRepository.update(given[1].getId(),given[1].getOrgName(), given[0].getId());
+            assertEquals(2, id);
+
+            Object[] actual = dslContext
+                    .selectFrom(organization)
+                    .fetchInto(Organization.class).toArray();
+            assertArrayEquals(new Organization[]{given[0], expected}, actual);
+        }
+
+        @Test
+        public void whenChangeHeadSelf_thenThrows() throws Exception {
+            Organization given = create();
+            WrongHeadIdException e = assertThrows(WrongHeadIdException.class,
+                    () -> organizationRepository.update(given.getId(), given.getOrgName(), given.getId()));
+            assertEquals( given.getId(), e.id);
+        }
+
+        @Test
+        public void whenWrongHead_thenThrows() throws Exception {
+            Organization given = create();
+            WrongHeadIdException e = assertThrows(WrongHeadIdException.class,
+                    () -> organizationRepository.update(given.getId(),given.getOrgName(), given.getId()+1));
+            assertEquals(given.getId(), e.id);
+        }
+
+        @Test
+        public void whenSameName_thenThrows() throws Exception {
+            Organization[] given = create(2);
+            SameNameException e = assertThrows(SameNameException.class,
+                    () -> organizationRepository.update(
+                            given[1].getId(),
+                            given[0].getOrgName(),
+                            given[1].getHeadOrgId()));
+            assertEquals(given[0].getOrgName(), e.name);
         }
     }
 
