f5221ba951c1e62bfa15a0372b4e85e2eb561e1e
Kokovin Aleksei <rycarok@gmail.com>
2020-06-29 19:02:11 +0500

    fix test

--- WebServer/src/test/java/ankokovin/fullstacktest/WebServer/OrganizationControllerTests.java
+++ WebServer/src/test/java/ankokovin/fullstacktest/WebServer/OrganizationControllerTests.java
@@ -3,7 +3,10 @@ package ankokovin.fullstacktest.WebServer;
 import ankokovin.fullstacktest.WebServer.Controllers.OrganizationsController;
 import ankokovin.fullstacktest.WebServer.Exceptions.SameNameException;
 import ankokovin.fullstacktest.WebServer.Generated.tables.pojos.Organization;
+import ankokovin.fullstacktest.WebServer.Models.CreateOrganizationInput;
 import ankokovin.fullstacktest.WebServer.Services.OrganizationService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.jooq.JSON;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -15,6 +18,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.nullValue;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -38,18 +42,20 @@ public class OrganizationControllerTests {
     @Test
     public void whenCreateCorrectNoHead_thenOrganizationCreates() throws Exception {
         String name = "Алексей";
-
+        CreateOrganizationInput input = new CreateOrganizationInput();
+        input.name = name;
         given(service.create(name, null))
                 .willReturn(new Organization(1,name, null));
 
+        ObjectMapper objectMapper = new ObjectMapper();
 
         mvc.perform(post("/api/organization")
                 .contentType(MediaType.APPLICATION_JSON)
-                .content(String.format("{'name':%s}", name)))
+                .content(objectMapper.writeValueAsString(input)))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.orgName", is(name)))
                 .andExpect(jsonPath("$.id", is(1)))
-                .andExpect(jsonPath("$.headOrgId", is(null)));
+                .andExpect(jsonPath("$.headOrgId", is(nullValue())));
 
     }
 }
