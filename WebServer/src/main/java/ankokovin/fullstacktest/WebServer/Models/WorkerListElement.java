ce2e4af14ba74f348814d326fadd749a1f94b588
Kokovin Aleksei <rycarok@gmail.com>
2020-06-29 12:55:19 +0500

    decompose org and head

--- WebServer/src/main/java/ankokovin/fullstacktest/WebServer/Models/WorkerListElement.java
+++ WebServer/src/main/java/ankokovin/fullstacktest/WebServer/Models/WorkerListElement.java
@@ -5,6 +5,8 @@ import ankokovin.fullstacktest.WebServer.Generated.tables.pojos.Worker;
 
 public class WorkerListElement {
     public String name;
-    public Organization org;
-    public Worker head;
+    public Integer org_id;
+    public String org_name;
+    public Integer head_id;
+    public String head_name;
 }
