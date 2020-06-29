19dfab0c4961f93863a4115d36d306ba725878e0
Kokovin Aleksei <rycarok@gmail.com>
2020-06-30 00:10:24 +0500

    add exception for wrong head id

---
+++ WebServer/src/main/java/ankokovin/fullstacktest/WebServer/Exceptions/WrongHeadIdException.java
@@ -0,0 +1,13 @@
+package ankokovin.fullstacktest.WebServer.Exceptions;
+
+public class WrongHeadIdException extends RuntimeException {
+
+    public Integer id;
+
+    public WrongHeadIdException(Integer id, String message, Throwable cause) {
+        super(message, cause);
+        this.id = id;
+    }
+    public WrongHeadIdException(Integer id, String message) { this(id,message,null); }
+    public WrongHeadIdException(Integer id) { this(id,null,null); }
+}
