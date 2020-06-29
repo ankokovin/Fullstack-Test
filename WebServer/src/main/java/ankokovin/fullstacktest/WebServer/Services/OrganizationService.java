823102d8606b1c086ab662767a239c0ecd53cac3
Kokovin Aleksei <rycarok@gmail.com>
2020-06-29 12:43:17 +0500

    mock services

--- WebServer/src/main/java/ankokovin/fullstacktest/WebServer/Services/OrganizationService.java
+++ WebServer/src/main/java/ankokovin/fullstacktest/WebServer/Services/OrganizationService.java
@@ -3,6 +3,8 @@ package ankokovin.fullstacktest.WebServer.Services;
 
 import ankokovin.fullstacktest.WebServer.Generated.tables.pojos.Organization;
 import ankokovin.fullstacktest.WebServer.Models.OrgListElement;
+import ankokovin.fullstacktest.WebServer.Models.TreeNode;
+import ankokovin.fullstacktest.WebServer.NotImplementedException;
 import ankokovin.fullstacktest.WebServer.Repos.OrganizationRepository;
 import org.jooq.Record2;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,12 +19,8 @@ public class OrganizationService
     @Autowired
     private OrganizationRepository rep;
 
-    public List<Organization> gelAll() {
-        return rep.getAll();
-    }
-
     public List<OrgListElement> getAllWithCount() {
-        List<OrgListElement> result = new ArrayList<OrgListElement>();
+        List<OrgListElement> result = new ArrayList<>();
         for (Record2<String, Integer> rec: rep.getAllWithCount()) {
             result.add(new OrgListElement(rec.component1(),rec.component2()));
         }
@@ -36,5 +34,17 @@ public class OrganizationService
 
     public Organization getById(Integer id) {
         return rep.getById(id);
+    }
+
+    public Organization update(Integer id, String name, Integer org_id) {
+        throw new NotImplementedException();
+    }
+
+    public Organization delete(Integer id) {
+        throw new NotImplementedException();
+    }
+
+    public TreeNode<Organization> getTree(Integer root, Integer depth) {
+        throw new NotImplementedException();
     }
 }
---
+++ WebServer/src/main/java/ankokovin/fullstacktest/WebServer/Services/WorkerService.java
@@ -0,0 +1,32 @@
+package ankokovin.fullstacktest.WebServer.Services;
+
+import ankokovin.fullstacktest.WebServer.Generated.tables.pojos.Organization;
+import ankokovin.fullstacktest.WebServer.Generated.tables.pojos.Worker;
+import ankokovin.fullstacktest.WebServer.Models.WorkerListElement;
+import ankokovin.fullstacktest.WebServer.NotImplementedException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class WorkerService {
+    public Worker create(String name, Integer org_id, Integer head_id) {
+        throw new NotImplementedException();
+    }
+
+    public Worker update(Integer id, String name, Integer org_id, Integer head_id) {
+        throw new NotImplementedException();
+    }
+
+    public Worker delete(Integer id) {
+        throw new NotImplementedException();
+    }
+
+    public List<WorkerListElement> get(Integer page, String searchName, String searchOrgName) {
+        throw new NotImplementedException();
+    }
+    public Worker getById(Integer id) {
+        throw new NotImplementedException();
+    }
+
+}
