cc4da663f1b2ca0a6287f5adc8f849798dd42c33
Kokovin Aleksei <rycarok@gmail.com>
2020-06-30 14:12:35 +0500

    refactors to nested test classes

--- WebServer/src/test/java/ankokovin/fullstacktest/WebServer/OrganizationServiceTests.java
+++ WebServer/src/test/java/ankokovin/fullstacktest/WebServer/OrganizationServiceTests.java
@@ -20,75 +20,85 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+import java.lang.annotation.*;
+
+
 public class OrganizationServiceTests {
 
-    @Autowired
-    private OrganizationService organizationService;
+    abstract  class OrganizationServiceTemplate {
+        @Autowired
+        protected OrganizationService organizationService;
 
-    @MockBean
-    private OrganizationRepository organizationRepository;
-
-    @BeforeEach
-    public void setUp() throws BaseException {
-        String name1 = "ООО Тест";
-        Mockito.when(organizationRepository.insert(name1, null))
-                .thenReturn(1)
-                .thenThrow(new SameNameException(name1));
-        Mockito.when(organizationRepository.getById(1))
-                .thenReturn(new Organization(1, name1, null));
-
-        String name2 = "ООО Тест-2";
-        Mockito.when(organizationRepository.insert(name2, 1))
-                .thenReturn(2)
-                .thenThrow(new SameNameException(name2));
-        Mockito.when(organizationRepository.getById(2))
-                .thenReturn(new Organization(2, name2, 1));
-
-        String name3 = "ООО Тест-3";
-        Mockito.when(organizationRepository.insert(name3, 1))
-                .thenReturn(3)
-                .thenThrow(new SameNameException(name3));
-        Mockito.when(organizationRepository.getById(3))
-                .thenReturn(new Organization(3, name3, 1));
-
-        Mockito.when(organizationRepository.insert(name3, 3))
-                .thenThrow(new WrongHeadIdException(3));
-
+        @MockBean
+        protected OrganizationRepository organizationRepository;
     }
 
-    @Test
-    public void whenCreateCorrectNoHead_thenOrganizationCreates() throws BaseException {
-        String name = "ООО Тест";
+    @Nested
+    @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+    class Create extends OrganizationServiceTemplate {
+        @BeforeEach
+        public void setUp() throws BaseException {
+            String name1 = "ООО Тест";
+            Mockito.when(organizationRepository.insert(name1, null))
+                    .thenReturn(1)
+                    .thenThrow(new SameNameException(name1));
+            Mockito.when(organizationRepository.getById(1))
+                    .thenReturn(new Organization(1, name1, null));
 
-        Organization actual = organizationService.create(new CreateOrganizationInput(name, null));
-        assertEquals(new Organization(1, name, null), actual);
-    }
+            String name2 = "ООО Тест-2";
+            Mockito.when(organizationRepository.insert(name2, 1))
+                    .thenReturn(2)
+                    .thenThrow(new SameNameException(name2));
+            Mockito.when(organizationRepository.getById(2))
+                    .thenReturn(new Organization(2, name2, 1));
 
-    @Test
-    public void whenCreateCorrectHead_thenOrganizationCreates() throws BaseException {
-        String name = "ООО Тест-2";
+            String name3 = "ООО Тест-3";
+            Mockito.when(organizationRepository.insert(name3, 1))
+                    .thenReturn(3)
+                    .thenThrow(new SameNameException(name3));
+            Mockito.when(organizationRepository.getById(3))
+                    .thenReturn(new Organization(3, name3, 1));
 
-        Organization actual = organizationService.create(new CreateOrganizationInput(name, 1));
-        assertEquals(new Organization(2, name, 1), actual);
-    }
+            Mockito.when(organizationRepository.insert(name3, 3))
+                    .thenThrow(new WrongHeadIdException(3));
 
-    @Test
-    public void whenCreateSameName_thenThrowsError() throws BaseException {
-        String name = "ООО Тест-3";
+        }
 
-        Organization actual_first = organizationService.create(new CreateOrganizationInput(name, 1));
-        assertEquals(new Organization(3, name, 1), actual_first);
+        @Test
+        public void whenCreateCorrectNoHead_thenOrganizationCreates() throws BaseException {
+            String name = "ООО Тест";
 
-        SameNameException exception = assertThrows(SameNameException.class,
-                () -> organizationService.create(new CreateOrganizationInput(name, 1)));
-        assertEquals(name, exception.name);
-    }
+            Organization actual = organizationService.create(new CreateOrganizationInput(name, null));
+            assertEquals(new Organization(1, name, null), actual);
+        }
 
-    public void whenCreateWrongHead_thenThrows() throws BaseException {
-        String name = "ООО Тест-3";
+        @Test
+        public void whenCreateCorrectHead_thenOrganizationCreates() throws BaseException {
+            String name = "ООО Тест-2";
 
-        Organization actual_first = organizationService.create(new CreateOrganizationInput(name, 1));
+            Organization actual = organizationService.create(new CreateOrganizationInput(name, 1));
+            assertEquals(new Organization(2, name, 1), actual);
+        }
+
+        @Test
+        public void whenCreateSameName_thenThrowsError() throws BaseException {
+            String name = "ООО Тест-3";
+
+            Organization actual_first = organizationService.create(new CreateOrganizationInput(name, 1));
+            assertEquals(new Organization(3, name, 1), actual_first);
+
+            SameNameException exception = assertThrows(SameNameException.class,
+                    () -> organizationService.create(new CreateOrganizationInput(name, 1)));
+            assertEquals(name, exception.name);
+        }
+
+        @Test
+        public void whenCreateWrongHead_thenThrows() {
+            String name = "ООО Тест-3";
+
+            WrongHeadIdException e = assertThrows(WrongHeadIdException.class,
+                    () -> organizationService.create(new CreateOrganizationInput(name, 3)));
+        }
     }
 
 }
