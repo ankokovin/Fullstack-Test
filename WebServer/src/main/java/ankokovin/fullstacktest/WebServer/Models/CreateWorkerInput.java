99d5ce23a06fc335de07e8626c5474fd7be3b216
Kokovin Aleksei <rycarok@gmail.com>
2020-06-30 23:50:18 +0500

    WorkerService.create tests

--- WebServer/src/main/java/ankokovin/fullstacktest/WebServer/Models/CreateWorkerInput.java
+++ WebServer/src/main/java/ankokovin/fullstacktest/WebServer/Models/CreateWorkerInput.java
@@ -4,4 +4,9 @@ public class CreateWorkerInput {
     public String name;
     public Integer org_id;
     public Integer head_id;
+    public CreateWorkerInput(String name, Integer org_id, Integer head_id) {
+        this.name = name;
+        this.org_id = org_id;
+        this.head_id = head_id;
+    }
 }
--- WebServer/src/test/java/ankokovin/fullstacktest/WebServer/WorkerServiceTests.java
+++ WebServer/src/test/java/ankokovin/fullstacktest/WebServer/WorkerServiceTests.java
@@ -1,9 +1,77 @@
 package ankokovin.fullstacktest.WebServer;
+import ankokovin.fullstacktest.WebServer.Exceptions.BaseException;
+import ankokovin.fullstacktest.WebServer.Exceptions.NoSuchRecordException;
+import ankokovin.fullstacktest.WebServer.Exceptions.UnexpectedException;
+import ankokovin.fullstacktest.WebServer.Exceptions.WrongHeadIdException;
+import ankokovin.fullstacktest.WebServer.Generated.tables.pojos.Worker;
+import ankokovin.fullstacktest.WebServer.Models.CreateWorkerInput;
+import ankokovin.fullstacktest.WebServer.Models.Table;
+import ankokovin.fullstacktest.WebServer.Repos.OrganizationRepository;
+import ankokovin.fullstacktest.WebServer.Repos.WorkerRepository;
+import ankokovin.fullstacktest.WebServer.Services.OrganizationService;
+import ankokovin.fullstacktest.WebServer.Services.WorkerService;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
 public class WorkerServiceTests {
 
+    @Autowired
+    protected WorkerService workerService;
+
+    @MockBean
+    protected WorkerRepository workerRepository;
+
+
+    @Nested
+    class Create{
+        @Test
+        void whenReturns_thenReturn() throws BaseException {
+            String name = "Test";
+            Integer id = 56;
+            Integer org_id = 42;
+            Integer head_id = 45;
+            Worker expected = new Worker(id, name, org_id, head_id);
+            Mockito.when(workerRepository.insert(name, org_id, head_id))
+                    .thenReturn(id);
+            Mockito.when(workerRepository.getById(id))
+                    .thenReturn(expected);
+            CreateWorkerInput input = new CreateWorkerInput(name, org_id, head_id);
+            Worker actual = workerService.create(input);
+            assertEquals(expected, actual);
+        }
+
+        @Test
+        void whenThrowsWrongHead_thenThrows() throws BaseException {
+            String name = "Test";
+            Integer org_id = 42;
+            Integer head_id = 45;
+            Mockito.when(workerRepository.insert(name,org_id,head_id))
+                    .thenThrow(new WrongHeadIdException(org_id, Table.WORKER));
+            WrongHeadIdException e = assertThrows(WrongHeadIdException.class,
+                    () -> workerService.create(new CreateWorkerInput(name, org_id, head_id)));
+            assertEquals(org_id, e.id);
+            assertEquals(Table.WORKER, e.to);
+        }
+        @Test
+        void whenThrowsUnknownException() throws BaseException {
+            String name = "Test";
+            Integer org_id = 42;
+            Integer head_id = 45;
+            String message = "TestTest";
+            Mockito.when(workerRepository.insert(name,org_id,head_id))
+                    .thenThrow(new UnexpectedException(message));
+            UnexpectedException e = assertThrows(UnexpectedException.class,
+                    () -> workerService.create(new CreateWorkerInput(name, org_id, head_id)));
+            assertEquals(message, e.getMessage());
+        }
+    }
 }
