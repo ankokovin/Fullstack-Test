3aef4bd2947df7a203951928cafc75f11e4abb18
= <=>
2020-07-04 22:30:59 +0500

    add tests for /api/organization DELETE

--- WebServer/src/test/java/ankokovin/fullstacktest/WebServer/TestHelpers/OrganizationHelpers.java
+++ WebServer/src/test/java/ankokovin/fullstacktest/WebServer/TestHelpers/OrganizationHelpers.java
@@ -1,6 +1,9 @@
 package ankokovin.fullstacktest.WebServer.TestHelpers;
 
 import ankokovin.fullstacktest.WebServer.Exceptions.BaseException;
+import ankokovin.fullstacktest.WebServer.Exceptions.SameNameException;
+import ankokovin.fullstacktest.WebServer.Exceptions.UnexpectedException;
+import ankokovin.fullstacktest.WebServer.Exceptions.WrongHeadIdException;
 import ankokovin.fullstacktest.WebServer.Generated.tables.pojos.Organization;
 import ankokovin.fullstacktest.WebServer.Models.TreeNode;
 import ankokovin.fullstacktest.WebServer.Repos.OrganizationRepository;
@@ -17,19 +20,21 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class OrganizationHelpers {
     private static final ankokovin.fullstacktest.WebServer.Generated.tables.Organization organization
             = ankokovin.fullstacktest.WebServer.Generated.tables.Organization.ORGANIZATION;
-    public static Organization[] create(int cnt, int offset,
-                                 String namePattern,
-                                 OrganizationRepository organizationRepository,
-                                 DSLContext dslContext) throws BaseException {
+
+    private static String defaultNamePattern = "ООО Тест%d";
+
+    public static Organization[] create(int cnt, int offset, Integer headOrgId, String namePattern,
+                                        OrganizationRepository organizationRepository, DSLContext dslContext)
+            throws BaseException {
         assert cnt >= 0;
         assert offset >= 0;
         assert namePattern != null;
         Organization[] expected = new Organization[cnt];
         for (int i = 0; i < cnt; ++i) {
             String fName = String.format(namePattern, i+offset);
-            int id = organizationRepository.insert(fName, null);
+            int id = organizationRepository.insert(fName, headOrgId);
             assertEquals(offset + i + 1, id);
-            expected[i] = new Organization(id, fName, null);
+            expected[i] = new Organization(id, fName, headOrgId);
         }
         Object[] actual = dslContext
                 .selectFrom(organization)
@@ -39,11 +44,23 @@ public class OrganizationHelpers {
         return expected;
     }
 
+    public static Organization[] create(int cnt, int offset, int headOrgId,
+                                        OrganizationRepository organizationRepository, DSLContext dslContext)
+            throws BaseException {
+        return create(cnt, offset, headOrgId, defaultNamePattern, organizationRepository, dslContext);
+    }
+
+    public static Organization[] create(int cnt, int offset,
+                                 String namePattern,
+                                 OrganizationRepository organizationRepository,
+                                 DSLContext dslContext) throws BaseException {
+       return create(cnt, offset, null, namePattern, organizationRepository, dslContext);
+    }
+
     public static Organization[] create(int cnt, int offset,
                                  OrganizationRepository organizationRepository,
                                  DSLContext dslContext) throws BaseException {
-        String name = "ООО Тест%d";
-        return create(cnt, offset, name, organizationRepository, dslContext);
+        return create(cnt, offset, defaultNamePattern, organizationRepository, dslContext);
     }
 
     public static Organization[] create(int cnt,
