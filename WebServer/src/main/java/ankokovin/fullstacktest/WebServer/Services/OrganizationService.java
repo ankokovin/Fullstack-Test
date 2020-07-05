33b097b1084befc25eee1ae380b611d34207fa16
= <=>
2020-07-05 21:38:27 +0500

    Добавлена документация для OrganizationService

--- WebServer/src/main/java/ankokovin/fullstacktest/WebServer/Services/OrganizationService.java
+++ WebServer/src/main/java/ankokovin/fullstacktest/WebServer/Services/OrganizationService.java
@@ -17,11 +17,21 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Сервис работы с организациями
+ */
 @Service
 public class OrganizationService {
     @Autowired
     private OrganizationRepository rep;
 
+    /**
+     * Получение информации об организациях с поддержкой поиска
+     * @param pageNum - номер страницы (нумерация с единицы)
+     * @param pageSize - количество записей на странице
+     * @param searchName - строка поиска
+     * @return Список информации об организациях
+     */
     public List<OrgListElement> getAllWithCount(int pageNum, int pageSize, String searchName) {
         List<OrgListElement> result = new ArrayList<>();
         for (Record4<Integer, String, Integer, Integer> rec : rep.getAllWithCount(pageNum, pageSize, searchName)) {
@@ -30,6 +40,14 @@ public class OrganizationService {
         return result;
     }
 
+    /**
+     * Создание организации
+     * @param model - входная информация об организации
+     * @return созданная организация
+     * @throws SameNameException - при наличии организации с данным именем
+     * @throws WrongHeadIdException - при указании несуществующего идентификатора головной организации
+     * @throws UnexpectedException - при неожиданной ошибке в ходе создания организации
+     */
     public Organization create(CreateOrganizationInput model) throws
             SameNameException,
             WrongHeadIdException,
@@ -42,10 +60,24 @@ public class OrganizationService {
         }
     }
 
+    /**
+     * Получение организации по идентификатору
+     * @param id идентификатор организации
+     * @return Организация
+     * @throws NoSuchRecordException - при отсутствии организации с данным идентификатором
+     */
     public Organization getById(Integer id) throws NoSuchRecordException {
         return rep.getById(id);
     }
 
+    /**
+     * Обновление данных организации
+     * @param model - входная информация об организации
+     * @return Изменённая организация
+     * @throws WrongHeadIdException - при указании несуществующего идентификатора головной организации или при цикличной зависимости
+     * @throws SameNameException - при наличии организации с данным именем
+     * @throws UnexpectedException - при неожиданной ошибке в ходе создания организации
+     */
     public Organization update(UpdateOrganizationInput model) throws
             WrongHeadIdException,
             SameNameException,
@@ -60,6 +92,14 @@ public class OrganizationService {
         }
     }
 
+    /**
+     * Удаление данных об организации
+     * @param id - идентификатор организации
+     * @return Удалённая организация
+     * @throws NoSuchRecordException - при отстутствии организации с данным идентификатором
+     * @throws DeleteHasChildException - при наличии дочерних организаций
+     * @throws UnexpectedException - при неожиданной ошибке в ходе удаления организации
+     */
     public Organization delete(Integer id) throws NoSuchRecordException, DeleteHasChildException, UnexpectedException {
         Organization res = getById(id);
         int res_id = rep.delete(id);
@@ -67,6 +107,13 @@ public class OrganizationService {
         return res;
     }
 
+    /**
+     * Получение информации об организациях в виде дерева
+     * @param root - идентификатор организации (может быть Null)
+     * @param depth - глубина поиска
+     * @return Дерево организаций
+     * @throws NoSuchRecordException - при отсутствии организации с данным идентификатором
+     */
     public TreeNode<Organization> getTree(Integer root, Integer depth) throws NoSuchRecordException {
         return rep.getTree(depth, root);
     }
