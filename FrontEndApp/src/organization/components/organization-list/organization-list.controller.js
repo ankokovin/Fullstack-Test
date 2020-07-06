d49957c8a4194905ea80bfc584063a51a42ae3cd
Aleksei Kokovin <rycarok@gmail.com>
2020-07-06 13:48:13 +0500

    temporary mock api get call with json file

--- FrontEndApp/src/organization/components/organization-list/organization-list.controller.js
+++ FrontEndApp/src/organization/components/organization-list/organization-list.controller.js
@@ -1,14 +1,6 @@
-export default function OrganizationListCtrl($scope) {
-    $scope.organizationList = [
-            {
-                'id': 1,
-                'name': 'Test1',
-                'count': 42
-            },{
-                'id': 2,
-                'name': 'Test2',
-                'count': 21
-            }
-        ];
-    
-    }
+export default function OrganizationListCtrl($scope, $http) {
+    $http.get('api/organization.json').then((response) => {
+        console.log(response);
+        $scope.organizationList = response.data;
+    });    
+}
--- FrontEndApp/src/organization/organization.module.js
+++ FrontEndApp/src/organization/organization.module.js
@@ -1,9 +1,11 @@
 import OrganizationListComponent from './components/organization-list/organization-list.component';
 import OrganizationListTreeComponent from './components/organization-list-tree/organization-list-tree.component';
 import OrganizationCreateComponent from './components/organization-create/organization-create.component';
+import OrganizationListCtrl from './components/organization-list/organization-list.controller';
+
 let OrganizationModule = angular.module('OrganizationModule',[]);
 
-OrganizationModule.component(OrganizationListComponent.componentName, OrganizationListComponent);
+OrganizationModule.component(OrganizationListComponent.componentName, OrganizationListComponent).controller('OrganizationListCtrl', OrganizationListCtrl);
 OrganizationModule.component(OrganizationListTreeComponent.componentName, OrganizationListTreeComponent);
 OrganizationModule.component(OrganizationCreateComponent.componentName, OrganizationCreateComponent);
 
