c5b80be8197bb691a88c2da0c056be2342c75911
Aleksei Kokovin <rycarok@gmail.com>
2020-07-06 22:07:05 +0500

    response checks and total page count calculation

--- FrontEndApp/src/organization/components/organization-list/organization-list.controller.js
+++ FrontEndApp/src/organization/components/organization-list/organization-list.controller.js
@@ -9,6 +9,9 @@ export default class OrganizationListCtrl{
             console.log(response);
             this.organizationList = response.data.list;
             this.total = response.data.total;
+            if (this.pageNum != response.data.page) alert('API returned wrong page');
+            if (this.pageSize != response.data.pageSize) alert('API returned wrong page size');
+            this.pageShowCnt = Math.ceil(this.total/this.pageSize);
         });    
     }
 }
