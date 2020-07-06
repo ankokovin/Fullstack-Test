cbc729a7e56a10e07c3b7be5612ad727cd582012
Aleksei Kokovin <rycarok@gmail.com>
2020-07-06 13:55:17 +0500

    extract route params

--- FrontEndApp/src/organization/components/organization-list/organization-list.controller.js
+++ FrontEndApp/src/organization/components/organization-list/organization-list.controller.js
@@ -1,4 +1,8 @@
-export default function OrganizationListCtrl($scope, $http) {
+export default function OrganizationListCtrl($scope, $http, $routeParams) {
+    console.log($routeParams);
+    let pageNum = 'pageNum' in $routeParams ? $routeParams['pageNum'] : '1'; 
+    let pageSize = 'pageSize' in $routeParams ? $routeParams['pageSize'] : '25';
+    let searchString = $routeParams['search'];
     $http.get('api/organization.json').then((response) => {
         console.log(response);
         $scope.organizationList = response.data;
