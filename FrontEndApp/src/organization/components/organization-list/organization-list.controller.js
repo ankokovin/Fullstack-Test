be1745862696a0048b74c1ee0e9e27af018aa65d
Aleksei Kokovin <rycarok@gmail.com>
2020-07-06 18:00:56 +0500

    use api for organization list

--- FrontEndApp/src/organization/components/organization-list/organization-list.controller.js
+++ FrontEndApp/src/organization/components/organization-list/organization-list.controller.js
@@ -1,10 +1,12 @@
 export default function OrganizationListCtrl($scope, $http, $routeParams) {
     "ngInject";
     console.log($routeParams);
-    let pageNum = 'pageNum' in $routeParams ? $routeParams['pageNum'] : '1'; 
-    let pageSize = 'pageSize' in $routeParams ? $routeParams['pageSize'] : '25';
-    let searchString = $routeParams['search'];
-    $http.get('api/organization.json').then((response) => {
+    let params = {
+        pageNum : 'pageNum' in $routeParams ? $routeParams['pageNum'] : '1',
+        pageSize : 'pageSize' in $routeParams ? $routeParams['pageSize'] : '25',
+        searchString : $routeParams['search']
+    }
+    $http.get('api/organization',{ params:params }).then((response) => {
         console.log(response);
         $scope.organizationList = response.data;
     });    
