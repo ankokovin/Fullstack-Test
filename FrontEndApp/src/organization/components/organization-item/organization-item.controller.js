723f4abd4e68eab67f0b11fe7632cd96a0fc20c7
Aleksei Kokovin <rycarok@gmail.com>
2020-07-07 23:11:24 +0500

    better dropdown

--- FrontEndApp/src/organization/components/organization-item/organization-item.controller.js
+++ FrontEndApp/src/organization/components/organization-item/organization-item.controller.js
@@ -8,21 +8,27 @@ export default class OrganizationItemCtrl{
         this.scope = $scope;
         this.load();
     }
+    select(id) {
+        console.log(id);
+        this.organization.head_id = id;
+        this.searchName = this.scope.organizationList[id];
+    }
 
     load() {
         //TODO: repeated code from organization-list: make a service
         let params = {
             page: 1,
             pageSize: 5,
-            orgName: this.organization.orgName
+            orgName: this.searchName
         };
         console.log(params)
         this.http.get('api/organization.json',{ params: params }).then((response) => {
             console.log(response);
-            this.scope.organizationList = response.data.list.slice(0,5);
+            this.scope.organizationList = Object.fromEntries(response.data.list.slice(0,5).map(x=>[x.id, x.name]));
+            console.log(this.scope.organizationList);
             if (response.data.page != 1) console.log('API returned wrong page');
             if (response.data.pageSize != 10) console.log('API returned wrong page size');
-            
+            this.organization.head_id = Object.keys(this.scope.organizationList).find(key => this.scope.organizationList[key] === this.searchName);
         });  
     }
 }
