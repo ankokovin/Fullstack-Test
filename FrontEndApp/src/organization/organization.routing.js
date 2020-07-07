e29b2594e9092ce4a2662a93aba04f6a6c20a8ed
Aleksei Kokovin <rycarok@gmail.com>
2020-07-08 02:37:41 +0500

    add route /organization/item/:id

--- FrontEndApp/src/organization/organization.routing.js
+++ FrontEndApp/src/organization/organization.routing.js
@@ -3,7 +3,9 @@ export default function($routeProvider){
         template: '<organization-list-component></organization-list-component>'
       }).when('/organization/tree-list', {
         template: '<organization-list-tree-component></organization-list-tree-component>'
-      }).when('/organization/item', {
+      }).when('/organization/item/', {
+        template: '<organization-item-component></organization-item-component>'
+      }).when('/organization/item/:id',{
         template: '<organization-item-component></organization-item-component>'
       });
 }
