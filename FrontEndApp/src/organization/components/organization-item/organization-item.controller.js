0a12019d60de8c2aafb0bba438e8392ddac1ecfe
Aleksei Kokovin <rycarok@gmail.com>
2020-07-08 01:48:49 +0500

    remade search without dropdown and fixed search

--- FrontEndApp/src/organization/components/organization-item/organization-item.controller.js
+++ FrontEndApp/src/organization/components/organization-item/organization-item.controller.js
@@ -6,7 +6,7 @@ export default class OrganizationItemCtrl{
         this.new =  'id' in $routeParams;
         this.scope = $scope;
         this.OrganizationService = OrganizationService;
-        this.load();
+        this.showList = false;
     }
     select(id) {
         console.log(id);
@@ -15,11 +15,14 @@ export default class OrganizationItemCtrl{
     }
 
     load() {
+        console.log(this.scope.organizationList);
+        console.log(this.searchName);
         this.OrganizationService.get_paged(1, 5, this.searchName).then((data) => {
             console.log(data);
             this.scope.organizationList = Object.fromEntries(data.list.slice(0,5).map(x=>[x.id, x.name]));
             console.log(this.scope.organizationList);
             this.organization.head_id = Object.keys(this.scope.organizationList).find(key => this.scope.organizationList[key] === this.searchName);
+            this.scope.$apply();
         });  
     }
 
@@ -28,5 +31,15 @@ export default class OrganizationItemCtrl{
         this.OrganizationService.create(this.organization.name, this.organization.head_id).then(
             (response) => console.log('Nice') //TODO: show some message, redirect?
         );
+    }
+
+    searchNameFocus() {
+        this.showList = true;
+        this.load();
+    }
+
+    searchNameBlur(event) {
+        console.log(event);
+        this.showList = false;
     }
 }
