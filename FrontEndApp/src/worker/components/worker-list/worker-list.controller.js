1cac01d21266f7f272cd780fff07b5b67acc81f0
Aleksei Kokovin <rycarok@gmail.com>
2020-07-08 21:24:33 +0500

    add search by both org and name

--- FrontEndApp/src/worker/components/worker-list/worker-list.controller.js
+++ FrontEndApp/src/worker/components/worker-list/worker-list.controller.js
@@ -4,8 +4,8 @@ export default class WorkerListCtrl{
         this.page = 'page' in $routeParams ? $routeParams['page'] : '1';
         this.pageSize = 'pageSize' in $routeParams ? $routeParams['pageSize'] : '25';
         this.searchName = $routeParams['search'];
-        
-        WorkerService.get_paged(this.page, this.pageSize, this.searchName).then((data) => {
+        this.searchOrgName = $routeParams['searchOrg'];
+        WorkerService.get_paged(this.page, this.pageSize, this.searchName, this.searchOrgName).then((data) => {
             this.workerList = data.list;
             this.total = data.total;
             this.pageShowCnt = Array(Math.ceil(this.total/this.pageSize));
