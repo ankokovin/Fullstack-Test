dc0c12129dd5b15c9503f7bf1785593aff9b9a0e
Aleksei Kokovin <rycarok@gmail.com>
2020-07-09 01:51:04 +0500

    just order by name is good enough

--- postgres/setup.sql
+++ postgres/setup.sql
@@ -16,34 +16,10 @@ CREATE TABLE Worker (
     CHECK (id != head_id)
 );
 
-CREATE OR REPLACE FUNCTION btrsort_nextunit(text) RETURNS text AS $$
-    SELECT 
-        CASE WHEN $1 ~ '^[^0-9]+' THEN
-            COALESCE( SUBSTR( $1, LENGTH(SUBSTRING($1 FROM '[^0-9]+'))+1 ), '' )
-        ELSE
-            COALESCE( SUBSTR( $1, LENGTH(SUBSTRING($1 FROM '[0-9]+'))+1 ), '' )
-        END
 
-$$ LANGUAGE SQL
-IMMUTABLE;
 
-CREATE OR REPLACE FUNCTION btrsort(text) RETURNS text AS $$
-    SELECT 
-        CASE WHEN char_length($1)>0 THEN
-            CASE WHEN $1 ~ '^[^0-9]+' THEN
-                RPAD(SUBSTR(COALESCE(SUBSTRING($1 FROM '^[^0-9]+'), ''), 1, 12), 12, ' ') || btrsort(btrsort_nextunit($1))
-            ELSE
-                LPAD(SUBSTR(COALESCE(SUBSTRING($1 FROM '^[0-9]+'), ''), 1, 12), 12, ' ') || btrsort(btrsort_nextunit($1))
-            END
-        ELSE
-            $1
-        END
-      ;
-$$ LANGUAGE SQL
-IMMUTABLE;
-
-CREATE INDEX ON Worker( btrsort(worker_name));
-CREATE INDEX ON Organization( btrsort(org_name));
+CREATE INDEX ON Worker(worker_name);
+CREATE INDEX ON Organization(org_name);
 
 
 
