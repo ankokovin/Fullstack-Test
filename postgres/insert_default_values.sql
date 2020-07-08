81d525130a6c16a98013ce12d937e144785feb3b
Aleksei Kokovin <rycarok@gmail.com>
2020-07-09 01:24:18 +0500

    generate lots of random values

--- postgres/insert_default_values.sql
+++ postgres/insert_default_values.sql
@@ -1,14 +1,52 @@
-INSERT INTO Organization VALUES
-    (1, 'headOrg1', NULL),
-    (2, 'headOrg2', NULL),
-    (3, 'Org_1_1', 1),
-    (4, 'Org_1_2', 1);
+CREATE OR REPLACE FUNCTION GenerateOrganization(count integer) RETURNS void AS $gen_org$
+    DECLARE
+        n integer;
+        n_range integer;
+        org_name text;
+        root_probability CONSTANT NUMERIC := 0.2;
+    BEGIN
+        FOR i IN 1..count LOOP
+            select count(*) into n_range from Organization;
+            n_range := n_range + 1;
+            n := floor(random()*n_range*(1+root_probability)-n_range*root_probability);
+            org_name := CONCAT('Test', n_range);
+            IF n <= 0 THEN 
+                INSERT INTO Organization VALUES (n_range, org_name, NULL);
+            ELSE
+                INSERT INTO Organization VALUES (n_range, org_name, n);
+            END IF;
+            END LOOP;
+    END;
+$gen_org$ LANGUAGE plpgsql;
 
-INSERT INTO Worker VALUES
-    (1, 'Worker1', 1, NULL),
-    (2, 'Worker2', 1, 1),
-    (3, 'Worker3', 2, NULL),
-    (4, 'Worker4', 2, 3),
-    (5, 'Worker5', 2, 4),
-    (6, 'Worker6', 3, NULL),
-    (7, 'Worker7', 4, NULL);
+CREATE OR REPLACE FUNCTION GenerateWorkers(n integer) RETURNS void AS $gen_worker$
+    DECLARE
+        org_id_v integer;
+        org_count integer;
+        head_probability NUMERIC := 0.1;
+        worker_cnt integer;
+        head_id integer;
+        head_id_n integer;
+        worker_name text;
+    BEGIN
+        SELECT(Count(*)) INTO org_count FROM Organization;
+        FOR i IN 1..n LOOP
+            org_id_v := floor(random()*org_count)+1;
+            SELECT(Count(*)) INTO worker_cnt FROM Worker WHERE Worker.org_id = org_id_v;
+            worker_name := CONCAT('Test', i);
+            IF worker_cnt > 0 AND random() < head_probability THEN
+                head_id_n := floor(random()*worker_cnt);
+                SELECT(Worker.id) INTO head_id  FROM Worker WHERE Worker.org_id = org_id_v OFFSET head_id_n LIMIT 1;
+                INSERT INTO Worker VALUES (i, worker_name, org_id_v, head_id);
+            ELSE
+                INSERT INTO Worker VALUES (i, worker_name, org_id_v, NULL);
+            END IF;
+        END LOOP;
+    END;
+$gen_worker$ LANGUAGE plpgsql;
+
+SELECT GenerateOrganization(20);
+
+SELECT GenerateWorkers(200);
+
+
