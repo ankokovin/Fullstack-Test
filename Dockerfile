eb0641f694a2c63a69ac1476c3a94fc03816c093
ankokovin <rycarok@gmail.com>
2020-07-09 20:18:41 +0500

    Dev/docker (#104)

--- Dockerfile
+++ Dockerfile
@@ -1,19 +1,16 @@
 FROM node:10.21.0-alpine3.11 as FrontEnd
-WORKDIR /app
-COPY FrontEndApp ./FrontEndApp
+COPY FrontEndApp /app/FrontEndApp
 WORKDIR /app/FrontEndApp
 RUN npm install
 RUN npm run-script build
 
 FROM ubuntu as BackEnd
 ENV DEBIAN_FRONTEND=noninteractive
-WORKDIR /app
-COPY WebServer ./WebServer
+COPY WebServer /app/WebServer
 WORKDIR /app/WebServer
 RUN apt-get update && apt-get install -y postgresql-12 maven sudo
 RUN useradd -m docker && echo "docker:docker" | chpasswd && adduser docker sudo
-
-COPY postgres/setup.sql app/postgres/setup.sql 
+COPY postgres/setup/setup.sql app/postgres/setup.sql 
 ENV PGPASSWORD 12345    
 RUN pg_ctlcluster 12 main start &&\
     sudo -u postgres psql -c "CREATE USER admin WITH PASSWORD '12345' SUPERUSER;" &&\
@@ -26,4 +23,5 @@ COPY --from=FrontEnd /app/WebServer/static /app/WebServer/target/static
 
 FROM openjdk:slim
 COPY --from=BackEnd /app/WebServer/target /app
-CMD java -jar /app/WebServer-0.0.1-SNAPSHOT.jar
+WORKDIR /app
+CMD java -jar WebServer-0.0.1-SNAPSHOT.jar
---
+++ postgres/setup/z_insert_default_values.sql
@@ -0,0 +1,52 @@
+CREATE OR REPLACE FUNCTION GenerateOrganization(count integer) RETURNS void AS $gen_org$
+    DECLARE
+        n integer;
+        n_range integer;
+        org_name text;
+        root_probability CONSTANT NUMERIC := 0.2;
+    BEGIN
+        FOR i IN 1..count LOOP
+            select count(*) into n_range from Organization;
+            n_range := n_range + 1;
+            n := floor(random()*n_range*(1+root_probability)-n_range*root_probability);
+            org_name := CONCAT('Test', n_range);
+            IF n <= 0 THEN 
+                INSERT INTO Organization VALUES (default, org_name, NULL);
+            ELSE
+                INSERT INTO Organization VALUES (default, org_name, n);
+            END IF;
+            END LOOP;
+    END;
+$gen_org$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION GenerateWorkers(n integer) RETURNS void AS $gen_worker$
+    DECLARE
+        org_id_v integer;
+        org_count integer;
+        head_probability NUMERIC := 0.1;
+        worker_cnt integer;
+        head_id integer;
+        head_id_n integer;
+        worker_name text;
+    BEGIN
+        SELECT(Count(*)) INTO org_count FROM Organization;
+        FOR i IN 1..n LOOP
+            org_id_v := floor(random()*org_count)+1;
+            SELECT(Count(*)) INTO worker_cnt FROM Worker WHERE Worker.org_id = org_id_v;
+            worker_name := CONCAT('Test', i);
+            IF worker_cnt > 0 AND random() > head_probability THEN
+                head_id_n := floor(random()*worker_cnt);
+                SELECT(Worker.id) INTO head_id  FROM Worker WHERE Worker.org_id = org_id_v OFFSET head_id_n LIMIT 1;
+                INSERT INTO Worker VALUES (default, worker_name, org_id_v, head_id);
+            ELSE
+                INSERT INTO Worker VALUES (default, worker_name, org_id_v, NULL);
+            END IF;
+        END LOOP;
+    END;
+$gen_worker$ LANGUAGE plpgsql;
+
+SELECT GenerateOrganization(20);
+
+SELECT GenerateWorkers(200);
+
+
